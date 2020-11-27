561d17a067d78b1bc759df6e817d0e7a09b256fc
AlexsandrSyx <rusk05@mail.ru>
2020-11-27 18:53:50 +0300

    Проверка получение БД в компаненте

--- src/main/java/ru/mtuci/simpleapi/CommandLineAppStartupRunner.java
+++ src/main/java/ru/mtuci/simpleapi/CommandLineAppStartupRunner.java
@@ -5,23 +5,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ru.mtuci.simpleapi.dao.ProductRepository;
 
-
 @Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
 
     private final ProductRepository productRepository;
-
 
     @Autowired
     public CommandLineAppStartupRunner(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-
-
-
     @Override
-    public void run(String...args) throws Exception{
-//        System.out.println(productRepository.findById(2L).get());
+    public void run(String... args) throws Exception {
+        System.out.println(productRepository.findById(2L).get());
     }
 }
